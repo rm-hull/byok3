3f51ae8b0d911132585043711c35e6d1c1d4d5c5
Richard Hull <rm_hull@yahoo.co.uk>
2017-08-03 23:04:51 +0100

    fish

---
+++ src/main/scala/byok3/data_structures/ExecutionToken.scala
@@ -0,0 +1,24 @@
+package byok3.data_structures
+
+import byok3.annonation.Documentation
+import byok3.data_structures.Context._
+import byok3.data_structures.Stack._
+import byok3.types.{Address, AppState, Data, Word}
+
+sealed trait ExecutionToken {
+  val name: Word
+  val effect: AppState[Unit]
+  val immediate: Boolean = false
+
+  def markImmediate: ExecutionToken = throw new UnsupportedOperationException()
+}
+
+case class Primitive(name: Word, effect: AppState[Unit], override val immediate: Boolean, doc: Option[Documentation]) extends ExecutionToken
+
+case class Constant(name: Word, value: Data) extends ExecutionToken {
+  override val effect = dataStack(push(value))
+}
+
+case class Variable(name: Word, addr: Address) extends ExecutionToken {
+  override val effect = dataStack(push(addr))
+}
