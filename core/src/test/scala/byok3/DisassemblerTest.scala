9f261fd8f9497e9ced36bcfa823a0b0ed94fdc53
Richard Hull <rm_hull@yahoo.co.uk>
2017-09-25 00:04:21 +0100

    Fix test

--- core/src/test/scala/byok3/DisassemblerTest.scala
+++ core/src/test/scala/byok3/DisassemblerTest.scala
@@ -19,13 +19,13 @@ class DisassemblerTest extends FunSuite {
   test("should print disassembly") {
     val expected =
       s"""${MID_GREY}00000124:  2B 00 00 00  |+...|  ${CYAN}${BOLD}: <unknown>${RESET}${MID_GREY}
-         |${MID_GREY}00000128:  64 00 00 00  |d...|  DUP
+         |${MID_GREY}00000128:  65 00 00 00  |e...|  DUP
          |${MID_GREY}0000012C:  02 00 00 00  |....|  *
          |${MID_GREY}00000130:  2C 00 00 00  |,...|  EXIT
          |${MID_GREY}00000134:  2B 00 00 00  |+...|  ${CYAN}${BOLD}: SQR${RESET}${MID_GREY}
-         |${MID_GREY}00000138:  49 00 00 00  |I...|  (LIT)
+         |${MID_GREY}00000138:  4A 00 00 00  |J...|  (LIT)
          |${MID_GREY}0000013C:  09 00 00 00  |....|  9
-         |${MID_GREY}00000140:  49 00 00 00  |I...|  (LIT)
+         |${MID_GREY}00000140:  4A 00 00 00  |J...|  (LIT)
          |${MID_GREY}00000144:  09 00 00 00  |....|  9
          |${MID_GREY}00000148:  02 00 00 00  |....|  *
          |${MID_GREY}0000014C:  2C 00 00 00  |,...|  EXIT
