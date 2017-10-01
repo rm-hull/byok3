b672d0d02799b571a62d95e990d44dbbaab59e33
Richard Hull <rm_hull@yahoo.co.uk>
2017-10-01 19:35:37 +0100

    HTerm implementation (#5)
    * Add hterm js
    * Readline control
    * Cursor control
    * Remove unused fonts
    * Move terminal style into CSS
    * Don't allow long lines of input
    * Update README with updated zeit host URL

--- web/src/main/resources/public/js/site.js
+++ web/src/main/resources/public/js/site.js
@@ -1,21 +1,19 @@
 "use strict";
 
-var scrollback = document.getElementById("scrollback");
-var input = document.getElementById("expression");
+String.prototype.replaceAll = function(search, replacement) {
+  var target = this;
+  return target.replace(new RegExp(search, 'g'), replacement);
+};
 
-function addLine(text) {
-  var elem = document.createElement('div');
-  elem.className = 'output';
-  elem.innerHTML = (text || ' ').replace(/</g,'&lt;').replace(/>/g,'&gt;');
-  scrollback.appendChild(elem);
-}
+String.prototype.deleteCharAt = function(pos) {
+  var target = this;
+  return (pos > 0) ? target.substring(0, pos - 1) + target.substring(pos) : target;
+};
 
-function clearScrollback() {
-  var children = scrollback.childNodes;
-  for (var i = 0, n = children.length; i < n; i++) {
-    scrollback.removeChild(children[0]);
-  }
-}
+String.prototype.insertCharAt = function(pos, char) {
+  var target = this;
+  return target.substring(0, pos) + char + target.substring(pos);
+};
 
 function busy(enable) {
   var elem = document.getElementById('busy')
@@ -26,7 +24,7 @@ function sendCommand(text, cb) {
   var xhr = new XMLHttpRequest();
 
   xhr.open('POST', '/byok3', true);
-  xhr.setRequestHeader('Accept', 'text/html');
+  xhr.setRequestHeader('Accept', 'text/plain');
   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
   xhr.onload = function() {
     cb(null, xhr.responseText);
@@ -38,27 +36,84 @@ function sendCommand(text, cb) {
   xhr.send(text);
 }
 
-document.onclick = function(evt) {
-  document.execCommand('copy');
-  input.focus();
-};
+function ESC(ansicode) {
+  return '\u001B[' + ansicode;
+}
 
-input.focus();
-input.onkeypress = function(event) {
-  if (event.which === 13) {
-    addLine(input.value);
+hterm.defaultStorage = new lib.Storage.Local();
+var t = new hterm.Terminal();
+t.onTerminalReady = function() {
+  var maxLineLength = 256;
+  var io = t.io.push();
+  var input = '';
+  var pos = 0;
+  var currHist = 0;
+  var history = [];
+  var insertMode = true;
+
+  io.onReadline = function(str) {
     busy(true);
-    sendCommand(input.value, function(err, result) {
-      addLine(result || err);
-      input.scrollIntoView({behaviour: 'instant', block: 'end'});
+    sendCommand(input, function(err, result) {
+      t.io.print((result || err).replaceAll('\n', '\r\n'));
       busy(false);
     });
-    input.value = '';
-    input.scrollIntoView({behaviour: 'instant', block: 'end'});
-  }
+  };
+
+  io.onVTKeystroke = io.sendString = function(str) {
+
+    // TODO - prevent input when busy
+    var chr = str.charCodeAt(0);
+    if (chr === 13) {
+
+      if (history.indexOf(input) < 0) {
+        history.push(input);
+        currHist = history.length;
+      }
+      io.onReadline(input);
+      io.println('');
+      input = '';
+      pos = 0;
+
+    } else if (chr === 127) {
+      input = input.deleteCharAt(pos)
+      pos -= 1;
+
+    } else if (str == ESC('A') && currHist > 0) {
+      currHist -= 1;
+      input = history[currHist];
+      pos = 0;
+
+    } else if (str == ESC('B') && currHist < history.length - 1) {
+      currHist += 1;
+      input = history[currHist];
+      pos = 0;
+
+    } else if (str === ESC('C') && pos < input.length) { // forward
+      pos += 1;
+
+    } else if (str === ESC('D') && pos > 0) { // backwards
+      pos -= 1;
+
+    } else if (chr >= 32 && chr <= 127 && pos < maxLineLength) {
+      if (insertMode) {
+        input = input.insertCharAt(pos, str);
+        pos += 1;
+      } else {
+        // TODO - replace mode
+      }
+    }
+
+    io.print('\r' + ESC('K') + input + '\r');
+    if (pos > 0) {
+      io.print(ESC(pos + 'C'));
+    }
+  };
+
+  sendCommand('', function(err, result) {
+    t.io.print((result || err).replaceAll('\n', '\r\n'));
+    busy(false);
+  });
 };
 
-sendCommand('', function(err, result) {
-  addLine(result || err);
-  busy(false);
-});
+t.decorate(document.querySelector('#terminal'));
+t.installKeyboard();
