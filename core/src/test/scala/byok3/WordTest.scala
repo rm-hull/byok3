a9485e260f522339b06f90872cc9d1c2567b74e5
Richard Hull <rm_hull@yahoo.co.uk>
2017-10-14 17:46:03 +0100

    Add ability to @IGNORE forth tests

--- core/src/test/scala/byok3/WordTest.scala
+++ core/src/test/scala/byok3/WordTest.scala
@@ -5,36 +5,45 @@ import java.io.File
 import byok3.data_structures.MachineState.OK
 import org.scalatest.{FunSuite, Matchers}
 
+import scala.io.Source
+
 class WordTest extends FunSuite with Matchers {
 
   var ctx = emptyContext
 
   test("Context should initialize correctly") {
+    // make sure system.fth is in a sane place first
     ctx.error shouldBe None
   }
 
-  def walkFolder(path: String)(block: String => Unit): Unit = {
+  walkFolder("/forth") { f =>
+    val name = s"${new File(f).getName}"
+    if (isMarkedAsIgnored(f)) ignore(name)(runForth(f)) else test(name)(runForth(f))
+  }
+
+  private def walkFolder(path: String)(block: String => Unit): Unit = {
     val folder = new File(getClass.getResource(path).getPath)
     if (folder.exists && folder.isDirectory)
       folder.listFiles
         .sorted
         .toList
-        .foreach(f => if (f.isDirectory) walkFolder(s"$path/${f.getName}")(block) else block(s"$path/${f.getName}"))
+        .foreach(f => if (f.isDirectory) walkFolder(s"$path/${f.getName}")(block) else block(s"$path/${f.getName}".substring(1)))
   }
 
-  walkFolder("/forth") { file =>
-    test(s"${new File(file).getName}") {
-      val res = capturingOutput {
-        ctx = ctx.eval(s"include ${file.substring(1)}")
-      }
-
-      if (res.contains("INCORRECT RESULT:") || res.contains("WRONG NUMBER OF RESULTS:"))
-        fail(res)
-
-      if (res.trim.nonEmpty)
-        println(res.trim)
-
-      ctx.status shouldBe Right(OK)
+  private def runForth(file: String) = {
+    val res = capturingOutput {
+      ctx = ctx.eval(s"include $file")
     }
+
+    if (res.contains("INCORRECT RESULT:") || res.contains("WRONG NUMBER OF RESULTS:"))
+      fail(res)
+
+    if (res.trim.nonEmpty)
+      println(res.trim)
+
+    ctx.status shouldBe Right(OK)
   }
+
+  private def isMarkedAsIgnored(file: String) =
+    Source.fromResource(file).getLines().toStream.exists(_.toUpperCase.contains("@IGNORED"))
 }
