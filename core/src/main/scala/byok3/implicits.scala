b752feea6a39849b300436774c01f57d36e5b422
Richard Hull <rm_hull@yahoo.co.uk>
2018-10-07 02:33:06 +0100

    Add rainbow string implicits
    Ripped off from: https://github.com/ktoso/scala-rainbow/pull/9

--- core/src/main/scala/byok3/implicits.scala
+++ core/src/main/scala/byok3/implicits.scala
@@ -59,4 +59,35 @@ package object implicits {
     }
   }
 
+  implicit class RainbowString(s: String) {
+
+    import Console._
+
+    def black = BLACK + s + RESET
+    def red = RED + s + RESET
+    def green = GREEN + s + RESET
+    def yellow = YELLOW + s + RESET
+    def blue = BLUE + s + RESET
+    def magenta = MAGENTA + s + RESET
+    def cyan = CYAN + s + RESET
+    def white = WHITE + s + RESET
+    def onBlack = BLACK_B + s + RESET
+    def onRed = RED_B + s + RESET
+    def onGreen = GREEN_B + s + RESET
+    def onYellow = YELLOW_B + s + RESET
+    def onBlue = BLUE_B + s + RESET
+    def onMagenta = MAGENTA_B + s + RESET
+    def onCyan = CYAN_B + s + RESET
+    def onWhite = WHITE_B + s + RESET
+    def bold = BOLD + s + RESET
+    def underlined = UNDERLINED + s + RESET
+    def blink = BLINK + s + RESET
+    def reversed = REVERSED + s + RESET
+    def invisible = INVISIBLE + s + RESET
+
+    /** ANSI256 */
+    def fg(i: Int)= s"\u001b[38;5;${i.abs % 256}m" + s + RESET
+    def bg(i: Int)= s"\u001b[48;5;${i.abs % 256}m" + s + RESET
+  }
+
 }
